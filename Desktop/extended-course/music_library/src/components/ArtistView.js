95d81486f9cf7c472d717fbfa25f4c26efbcdb9e
Jason Garcia <jaygarcia0530@gmail.com>
2022-04-25 20:04:03 -0700

    Activity 6 pending

--- Desktop/extended-course/music_library/src/components/ArtistView.js
+++ Desktop/extended-course/music_library/src/components/ArtistView.js
@@ -1,16 +1,37 @@
-import {useState, useEffect} from 'react'
-import {useParams} from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
-function ArtistView(){
-    const [artistData, setArtistData] = useState([])
-    const {id} = useParams()
+function ArtistView() {
+    const { id } = useParams()
+    const [ artistData, setArtistData ] = useState([])
 
-    return(
-        <div>
-            <h2>The id passed was: {id} </h2>
-            <p>Artist Data Goes Here!</p>
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/album/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setArtistData(resData.results)
+        }
+        fetchData()
+    }, {id})
+
+    const justAlbums = artistData.filter(entry => entry.collectionType === 'Album')
+
+    const renderAlbums = justAlbums.map((album, i) => {
+        return (
+            <div>
+                <p>{album.collectionName}</p>
+            </div>
+        )
+    })
+    
+    return (
+        <div key={id}>
+            {renderAlbums}
         </div>
     )
 }
+    
 
 export default ArtistView
