6b895bd0978bba398c3b1d31a7b5b657a39e9631
Jason Garcia <jaygarcia0530@gmail.com>
2022-04-27 19:23:23 -0700

    test commit

--- Desktop/extended-course/music_library/src/components/ArtistView.js
+++ Desktop/extended-course/music_library/src/components/ArtistView.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import {Link} from 'react-router-dom'
 import {useNavigate} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 function ArtistView() {
     const navigate = useNavigate()
