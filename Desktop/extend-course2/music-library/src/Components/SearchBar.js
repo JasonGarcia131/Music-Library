672ea52571f48096b7efeef1ae70d789da18721f
Jason Garcia <jaygarcia0530@gmail.com>
2022-04-29 19:06:33 -0700

    Refactored context

--- Desktop/extend-course2/music-library/src/Components/SearchBar.js
+++ Desktop/extend-course2/music-library/src/Components/SearchBar.js
@@ -1,13 +1,14 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
+import {SearchContext} from '../Context/SearchContext.js'
 
-function SearchBar(props){
-    let [searchTerm, setSearchTerm] = useState('')
+function SearchBar(){
+    const {term, handleSearch} = useContext(SearchContext)
 
     return (
         // onSubmit={(e) => props.handleSearch(e, searchTerm)}
          <form> 
-            <input type="text" placeholder="Enter a search term here"
-            onChange={(e) => props.handleSearch(e, e.target.value)} />
+            <input ref={term} type="text" placeholder="Enter a search term here"/>
+            <button onClick={(e) => handleSearch(e, term.current.value)}>Submit</button>
         </form>
     )
 }
---
+++ Desktop/extend-course2/music-library/src/Context/SearchContext.js
@@ -0,0 +1,7 @@
+import { createContext, useContext } from "react";
+
+export const SearchContext = createContext({
+    term: '',
+    handleSearch: () => {}
+})
+
