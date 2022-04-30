1ffac5573c45a0499f5310afd438c1acb69b1620
Jason Garcia <jaygarcia0530@gmail.com>
2022-04-29 18:41:08 -0700

    Added data context  provider

--- Desktop/extend-course2/music-library/src/App.js
+++ Desktop/extend-course2/music-library/src/App.js
@@ -1,7 +1,7 @@
 import Gallery from './Components/Gallery';
 import SearchBar from './Components/SearchBar';
 import React, {useEffect, useState} from 'react'
-
+import { DataContext } from './Context/DataContext';
 
 const App = () => {
   let [search, setSearch] = useState('')
@@ -35,7 +35,10 @@ const App = () => {
       <div>
           <SearchBar handleSearch={handleSearch} />
           {message}
-          <Gallery data={data} />
+          <DataContext.Provider value={data}>
+            <Gallery/>
+          </DataContext.Provider>
+          
       </div>
   )
 }
--- Desktop/extend-course2/music-library/src/Components/Gallery.js
+++ Desktop/extend-course2/music-library/src/Components/Gallery.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import GalleryItem from './GalleryItem'
+import {useContext} from 'react'
+import {DataContext} from '../Context/DataContext.js'
 
 
-function Gallery(props){
+function Gallery(){
 
-    const display = props.data.map((item, index) => {
+    const data = useContext(DataContext)
+
+    const display = data.map((item, index) => {
         return (
             <GalleryItem item={item} key={index} />
         )
